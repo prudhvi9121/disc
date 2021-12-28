62cd9629b70adac4e9e97f9a399e3d23d0a62f0c
prudhvi9121 <96787579+prudhvi9121@users.noreply.github.com>
2021-12-28 22:01:55 +0530

    Add files via upload

---
+++ cicle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	float r,a,p;
+	printf("enter radius");
+	scanf("%f",&r);
+	a=3.14*r*r;
+	p=2*3.14*r;
+	printf("area of circle is %f\n perimeter of circle  is %f",a,p);
+}
